d948b0aef3981947f4e43e8f0d0ba43d1732caf9
Adefolami <akintokunadefolami@gmail.com>
2024-06-07 23:44:29 +0100

    implemented smooth scrolling for faq section

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ const collectionStn = document.querySelector(".section-collections");
 collectionBtn.addEventListener("click", function (e) {
   collectionStn.scrollIntoView({ behavior: "smooth" });
 });
+
+const faqBtn = document.querySelector("#header__navigation-faq");
+const faqStn = document.querySelector(".section-faq");
+
+faqBtn.addEventListener("click", function (e) {
+  faqStn.scrollIntoView({ behavior: "smooth" });
+});
