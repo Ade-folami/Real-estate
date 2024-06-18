80bcdc4630fdf9c3f219e7727d31a110cd52a517
Adefolami <akintokunadefolami@gmail.com>
2024-06-18 23:01:26 +0100

    fixing bug in smooth scroll

--- script.js
+++ script.js
@@ -2,18 +2,16 @@
 
 const collectionBtn = document.querySelector("#header__navigation-collection");
 const collectionStn = document.querySelector(".section-collections");
-
-collectionBtn.addEventListener("click", function (e) {
-  collectionStn.scrollIntoView({ behavior: "smooth" });
-});
-
 const faqBtn = document.querySelector("#header__navigation-faq");
 const faqStn = document.querySelector(".section-faq");
 
-faqBtn.addEventListener("click", function (e) {
-  faqStn.scrollIntoView({ behavior: "smooth" });
+//Implementing smooth scrolling
+collectionBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  collectionStn.scrollIntoView({ behavior: "smooth" });
 });
 
-if (module.hot) {
-  module.hot.accept();
-}
+faqBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  faqStn.scrollIntoView({ behavior: "smooth" });
+});
